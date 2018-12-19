226151b6d37b33ca9c5d9c04806af502eee2e002
bak0903 <bak0903@mail.ru>
2018-12-19 19:51:50 +0600

    ListView and DetailView

--- blog/webapp/views.py
+++ blog/webapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from webapp.models import Blogger, Article, Favorite
 
-# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class BloggerListView(ListView):
+    model = Blogger
+    template_name = 'blogger_list.html'
+
+
+class BloggerDetailView(DetailView):
+    model = Blogger
+    template_name = 'blogger_detail.html'
+
+
+class FavoriteListView(ListView):
+    model = Favorite
+    template_name = 'favorite_list.html'
